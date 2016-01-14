133829b4b0a9b61f20fe5e4108f60a89cbd9bac3
Taku <bakednt@gmail.com>
2016-01-15 05:31:58 +0900

    changed.

--- conf/web-mode-setting.el
+++ conf/web-mode-setting.el
@@ -16,8 +16,12 @@
 (require 'company-web)
 (require 'company-web-html)
 (require 'company-web-slim)
-(add-to-list 'company-backends 'company-web-html)
-(add-to-list 'company-backends 'company-web-slim)
+(add-hook 'web-mode-hook (lambda ()
+                           (set (make-local-variable 'company-backends) '(company-web-html))
+                           (company-mode t)))
+(add-hook 'slim-mode-hook (lambda ()
+                           (set (make-local-variable 'company-backends) '(company-web-slim))
+                           (company-mode t)))
 
 (provide 'web-mode-setting)
 ;;; web-mode-setting.el ends here
