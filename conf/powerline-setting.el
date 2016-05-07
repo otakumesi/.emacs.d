a703b0d0a3c0c673d87a1dbe29eb97ab9880e162
Taku <takelow1994@me.com>
2016-05-07 20:08:14 +0900

    modifiy

--- conf/powerline-setting.el
+++ conf/powerline-setting.el
@@ -10,6 +10,22 @@
  '(mode-line ((t (:foreground "#002b36" :background "#268bd2" :box nil))))
  '(mode-line-inactive ((t (:foreground "#002b36" :background "#268bd2" :box nil)))))
 
+(set-face-attribute 'mode-line nil
+                    :overline  "#268bd2"
+                    :foreground "#002b36"
+                    :background "#268bd2"
+                    :box nil)
+
+(set-face-attribute 'mode-line-buffer-id nil
+                    :foreground "#002b36"
+                    :background "#268bd2"
+                    :inherit 'mode-line)
+
+(set-face-attribute 'modeline-inactive nil
+                    :foreground "#002b36"
+                    :background "#268bd2"
+                    :inherit 'mode-line)
+
 (set-face-attribute 'modeline-highlight nil
                     :foreground "#002b36"
                     :background "#6c71c4"
@@ -35,21 +51,6 @@
                     :background "#859900"
                     :inherit 'mode-line)
 
-(set-face-attribute 'mode-line nil
-                    :overline  "#268bd2"
-                    :foreground "#002b36"
-                    :background "#268bd2"
-                    :box nil)
-
-(set-face-attribute 'mode-line-buffer-id nil
-                    :foreground "#002b36"
-                    :background "#268bd2"
-                    :inherit 'mode-line)
-
-(set-face-attribute 'modeline-inactive nil
-                    :foreground "#002b36"
-                    :background "#268bd2"
-                    :inherit 'mode-line)
 
 (provide 'powerline-setting)
 ;;; powerline-setting.el ends here
