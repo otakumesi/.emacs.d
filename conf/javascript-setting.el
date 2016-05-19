4773320a5315029a1d3d036cc0ae5732d457ceac
Taku <bakednt@gmail.com>
2016-05-19 23:27:58 +0900

    add

--- conf/javascript-setting.el
+++ conf/javascript-setting.el
@@ -8,16 +8,16 @@
 (add-hook 'js2-mode-hook 'js2-refactor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-js))
+(add-to-list 'auto-mode-alist '("\\.jsx" . js2-js))
 (flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
 
-(add-to-list 'auto-mode-alist '("\\.jade\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jade'" . js2-mode))
 (autoload 'jade-mode "jade-mode")
 (autoload 'coffee-mode "coffee-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.coffee" . coffee-mode))
 
 (autoload 'json-mode "json-mode")
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.json'" . json-mode))
 
 (with-eval-after-load 'js2-mode
   (flycheck-add-next-checker 'javascript-jshint
