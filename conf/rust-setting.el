0c5a713a91cb5159d5dcd0992cbe5266c0dcd58a
Taku <bakednt@gmail.com>
2016-11-08 21:24:48 +0900

    dd

---
+++ conf/rust-setting.el
@@ -0,0 +1,31 @@
+(el-get-bundle rust-mode)
+(require 'rust-mode)
+
+(el-get-bundle toml-mode)
+(require 'toml-mode)
+
+(el-get-bundle racer-rust/emacs-racer)
+(require 'racer)
+
+(setq racer-rust-src-path "/Users/niitsumatakurou/workspace/rust/src")
+
+(add-hook 'rust-mode-hook #'racer-mode)
+
+(el-get-bundle company-racer)
+(require 'company-racer)
+
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
+(el-get-bundle cargo)
+(require 'cargo)
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+(el-get-bundle flycheck-rust)
+(require 'flycheck-rust)
+
+(add-hook 'rust-mode-hook #'rustfmt-enable-on-save)
+(add-hook 'rust-mode-hook #'flycheck-rust-setup)
+
+(el-get-bundle rustfmt)
+(require 'rustfmt)
